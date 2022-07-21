9e61199e87248e6b760dd9f90be6e05def60bcd7
GroovyCj <hairston917@gmail.com>
2022-07-21 15:10:18 -0700

    AB#66, AB#65, added file reader and GameInfo/Rooms

--- src/main/java/com/blackforestcastle/client/Main.java
+++ src/main/java/com/blackforestcastle/client/Main.java
@@ -1,11 +1,29 @@
 package com.blackforestcastle.client;
 
-import com.blackforestcastle.Commands;
-import com.blackforestcastle.Controller;
+import com.blackforestcastle.*;
+
+import java.io.IOException;
 
 class Main {
-    public static void main(String[] args) {
-        Controller controller = Controller.getInstance();
+    public static void main(String[] args) throws IOException {
+        /*Controller controller = Controller.getInstance();
         controller.newGame();
+
+         */
+        JSONReader j = new JSONReader();
+        //j.getRooms();
+        Room[] m = j.getR();
+        for (Room rm : m){
+            System.out.println(rm.roomInfo());
+        }
+
+        GameInfo[] gInfo = j.getGameInfo();
+        System.out.println(gInfo[0].getInfo());
+
+
+
+
+
+
     }
 }
